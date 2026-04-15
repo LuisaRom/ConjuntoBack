62751b7613c92ea6dbf638e563bc9be181669587
LuisaRom <lufromero13@gmail.com>
2026-04-15 16:39:42 -0500

    mamadax2

--- src/main/java/com/example/APP/AppApplication.java
+++ src/main/java/com/example/APP/AppApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class dAppApplication {
+public class AppApplication {
 
 	private static final Logger log = LoggerFactory.getLogger(AppApplication.class);
 
